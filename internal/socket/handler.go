a0d5ac90d547d2d5882d2a54eb74babe6db964c2
hongker <xiaok2013@live.com>
2022-10-26 21:40:15 +0800

    feat: add message

--- internal/socket/handler.go
+++ internal/socket/handler.go
@@ -88,44 +88,7 @@ func (handler *Handler) heartbeat(ctx *znet.Context, req *api.HeartbeatRequest) 
 	return
 }
 
-func (handler *Handler) listSession(ctx *znet.Context, req *api.SessionListRequest) (resp *api.SessionListResponse, err error) {
-	uid := handler.currentUser(ctx)
-	sessions, err := handler.sessionApp.GetSessionList(ctx, uid)
-	if err != nil {
-		return
-	}
-
-	resp = &api.SessionListResponse{Items: make([]api.Session, 0, len(sessions))}
-	for _, session := range sessions {
-		resp.Items = append(resp.Items, api.Session{ID: session.ID, Title: session.Title})
-	}
-	return
-}
-
-func (handler *Handler) sendMessage(ctx *znet.Context, req *api.MessageSendRequest) (resp *api.MessageSendResponse, err error) {
-	uid := handler.currentUser(ctx)
-	if err != nil {
-		return
-	}
-	msg := &application.Message{
-		Content:     req.Content,
-		ContentType: req.ContentType,
-		Target:      req.Target,
-		Sender:      uid,
-	}
-
-	packet := &codec.Packet{Header: codec.Header{Operate: api.OperatePushMessage, ContentType: ctx.Request().Header.ContentType}}
-	err = handler.messageApp.Send(ctx, msg, codec.Default(), packet)
-	if err == nil {
-		// save user session
-		handler.sessionApp.SaveSession(ctx, uid, &application.Session{ID: req.Target, Title: "", Last: msg})
-		handler.sessionApp.SaveSession(ctx, req.Target, &application.Session{ID: uid, Title: "", Last: msg})
-	}
-	return
-}
-
 func (handler *Handler) createChannel(ctx *znet.Context, req *api.ChannelCreateRequest) (resp *api.ChannelCreateResponse, err error) {
-
 	uid := handler.currentUser(ctx)
 	channel, err := handler.channelApp.Create(ctx, uid, req.Name)
 	if err != nil {
---
+++ internal/socket/message.go
@@ -0,0 +1,46 @@
+package socket
+
+import (
+	"github.com/ebar-go/znet"
+	"github.com/ebar-go/znet/codec"
+	"gochat/api"
+	"gochat/internal/application"
+)
+
+// listSession return a list of user sessions
+func (handler *Handler) listSession(ctx *znet.Context, req *api.SessionListRequest) (resp *api.SessionListResponse, err error) {
+	uid := handler.currentUser(ctx)
+	sessions, err := handler.sessionApp.GetSessionList(ctx, uid)
+	if err != nil {
+		return
+	}
+
+	resp = &api.SessionListResponse{Items: make([]api.Session, 0, len(sessions))}
+	for _, session := range sessions {
+		resp.Items = append(resp.Items, api.Session{ID: session.ID, Title: session.Title})
+	}
+	return
+}
+
+// sendMessage sends a message to receiver
+func (handler *Handler) sendMessage(ctx *znet.Context, req *api.MessageSendRequest) (resp *api.MessageSendResponse, err error) {
+	uid := handler.currentUser(ctx)
+	if err != nil {
+		return
+	}
+	msg := &application.Message{
+		Content:     req.Content,
+		ContentType: req.ContentType,
+		Target:      req.Target,
+		Sender:      uid,
+	}
+
+	packet := &codec.Packet{Header: codec.Header{Operate: api.OperatePushMessage, ContentType: ctx.Request().Header.ContentType}}
+	err = handler.messageApp.Send(ctx, msg, codec.Default(), packet)
+	if err == nil {
+		// save user session
+		handler.sessionApp.SaveSession(ctx, uid, &application.Session{ID: req.Target, Title: "", Last: msg})
+		handler.sessionApp.SaveSession(ctx, req.Target, &application.Session{ID: uid, Title: "", Last: msg})
+	}
+	return
+}
