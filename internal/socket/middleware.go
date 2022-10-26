1c8bb95ff9e9b005e3cb8d55aece28f3c3abb4b8
hongker <xiaok2013@live.com>
2022-10-26 21:26:59 +0800

    feat: add middleware

---
+++ internal/socket/middleware.go
@@ -0,0 +1,31 @@
+package socket
+
+import (
+	"github.com/ebar-go/ego/component"
+	"github.com/ebar-go/znet"
+	"gochat/api"
+)
+
+// WriteRequestLog represents print request log middleware
+func (handler *Handler) WriteRequestLog(ctx *znet.Context) {
+	component.Provider().Logger().Infof("[%s] request log: header=%+v, body=%v ", ctx.Conn().ID(), ctx.Request().Header, string(ctx.Request().Body))
+	ctx.Next()
+}
+
+// CheckLogin validates the login credentials
+func (handler *Handler) CheckLogin(ctx *znet.Context) {
+	// skip when operate is Login
+	if ctx.Request().Header.Operate == api.OperateLogin {
+		ctx.Next()
+		return
+	}
+
+	//
+	_, err := handler.getCurrentUser(ctx)
+	if err != nil {
+		component.Provider().Logger().Error(err.Error())
+		ctx.Abort()
+		return
+	}
+	ctx.Next()
+}
